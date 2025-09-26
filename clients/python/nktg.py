5c511e411b05b55c8547bd738026e476e3639266
NKTgLaw <traiphieu.com@gmail.com>
2025-09-26 22:34:48 +0700

    Update nktg.py

--- clients/python/nktg.py
+++ clients/python/nktg.py
@@ -1,1 +1,7 @@
+import requests
+
+def compute_nktg(x, v, m, dm_dt):
+    payload = {"x": x, "v": v, "m": m, "dm_dt": dm_dt}
+    r = requests.post("http://localhost:8080/nktg", json=payload)
+    return r.json()
 
