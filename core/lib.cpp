388e01d89249329f7f357d26019634bedb6b71ac
NKTgLaw <traiphieu.com@gmail.com>
2025-09-26 22:26:27 +0700

    Update lib.cpp

--- core/lib.cpp
+++ core/lib.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "nktg.hpp"
+
+int main() {
+    double x = 2.0, v = 3.0, m = 5.0, dm_dt = 0.1;
+
+    double p = nktg::momentum(m, v);
+    double NKTg1 = nktg::computeNKTg1(x, p);
+    double NKTg2 = nktg::computeNKTg2(dm_dt, p);
+
+    std::cout << "p=" << p << " NKTg1=" << NKTg1 << " NKTg2=" << NKTg2 << std::endl;
+    return 0;
+}
 
