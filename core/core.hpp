387f8f0c7babdbf679b148818ace0bea293cb949
NKTgLaw <traiphieu.com@gmail.com>
2025-09-26 22:17:28 +0700

    Update core.hpp

--- core/core.hpp
+++ core/core.hpp
@@ -1,1 +1,24 @@
+#ifndef NKTG_HPP
+#define NKTG_HPP
+
+namespace nktg {
+
+    // Tính động lượng
+    inline double momentum(double m, double v) {
+        return m * v;
+    }
+
+    // Tính NKTg₁ = x × p
+    inline double computeNKTg1(double x, double p) {
+        return x * p;
+    }
+
+    // Tính NKTg₂ = (dm/dt) × p
+    inline double computeNKTg2(double dm_dt, double p) {
+        return dm_dt * p;
+    }
+
+}
+
+#endif
 
