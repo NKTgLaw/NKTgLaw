c955faf4bf1877f3ee322d90b44b1bb09b62f561
NKTgLaw <traiphieu.com@gmail.com>
2025-09-27 01:16:21 +0700

    Update app.py

--- server/app.py
+++ server/app.py
@@ -5,17 +5,23 @@ app = Flask(__name__)
 @app.route('/api/momentum', methods=['POST'])
 def momentum():
     data = request.get_json()
-    return jsonify({"momentum": data["mass"] * data["velocity"]})
+    mass = data.get('mass', 0)
+    velocity = data.get('velocity', 0)
+    return jsonify({"momentum": mass * velocity})
 
 @app.route('/api/nktg1', methods=['POST'])
 def nktg1():
     data = request.get_json()
-    return jsonify({"nktg1": data["x"] * data["momentum"]})
+    x = data.get('x', 0)
+    p = data.get('momentum', 0)
+    return jsonify({"nktg1": x * p})
 
 @app.route('/api/nktg2', methods=['POST'])
 def nktg2():
     data = request.get_json()
-    return jsonify({"nktg2": data["dm_dt"] * data["momentum"]})
+    dm_dt = data.get('dm_dt', 0)
+    p = data.get('momentum', 0)
+    return jsonify({"nktg2": dm_dt * p})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
