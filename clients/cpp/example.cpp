ddf02fffda382b53eaab1b5694de9f72d1e86d1c
NKTgLaw <traiphieu.com@gmail.com>
2025-09-27 11:43:38 +0700

    Update example.cpp

--- clients/cpp/example.cpp
+++ clients/cpp/example.cpp
@@ -1,9 +1,11 @@
-#include "NKTgClient.cpp"
+#include "nktg_law/NKTgClient.cpp"
 
 int main() {
     NKTgClient client(2.0, 3.0, 5.0, 0.1);
+
     std::cout << "Momentum: " << client.momentum() << std::endl;
     std::cout << "NKTg₁: " << client.nktg1() << std::endl;
     std::cout << "NKTg₂: " << client.nktg2() << std::endl;
+
     return 0;
 }
