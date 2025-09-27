721a81b8b196b7df88497a6d3ea1c1b851275eab
NKTgLaw <traiphieu.com@gmail.com>
2025-09-27 11:42:56 +0700

    Update NKTgClient.cpp

--- clients/cpp/NKTgClient.cpp
+++ clients/cpp/NKTgClient.cpp
@@ -7,15 +7,15 @@ public:
     NKTgClient(double x_, double v_, double m_, double dm_dt_)
         : x(x_), v(v_), m(m_), dm_dt(dm_dt_) {}
 
-    double momentum() {
+    double momentum() const {
         return m * v;
     }
 
-    double nktg1() {
+    double nktg1() const {
         return momentum() + dm_dt * x;
     }
 
-    double nktg2() {
+    double nktg2() const {
         return nktg1() / m;
     }
 };
