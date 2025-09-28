5772b0dbf05d2be81473d92ce7878ef2caf6bd10
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 16:41:10 +0700

    Create javascript_snippets.js

---
+++ examples/javascript_snippets.js
@@ -0,0 +1,4 @@
+// JavaScript: web front-end and back-end
+const x = 2, v = 3, m = 5, dm_dt = 0.1;
+const p = m * v, NKTg1 = x * p, NKTg2 = dm_dt * p;
+console.log(`p=${p} NKTg1=${NKTg1} NKTg2=${NKTg2}`);
