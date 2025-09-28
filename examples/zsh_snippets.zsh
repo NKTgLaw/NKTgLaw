79ceac5caa1a4a1e06988809cfaf72e50217e284
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 22:19:35 +0700

    Create zsh_snippets.zsh

---
+++ examples/zsh_snippets.zsh
@@ -0,0 +1,9 @@
+# Zsh: Unix shell scripting
+x=2.0
+v=3.0
+m=5.0
+dm_dt=0.1
+p=$(echo "$m * $v" | bc)
+NKTg1=$(echo "$x * $p" | bc)
+NKTg2=$(echo "$dm_dt * $p" | bc)
+echo "p=$p NKTg1=$NKTg1 NKTg2=$NKTg2"
