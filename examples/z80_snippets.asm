baec0331d4050dbcb307e27e071c2e28f0ba3c20
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 22:22:41 +0700

    Create z80_snippets.asm

---
+++ examples/z80_snippets.asm
@@ -0,0 +1,4 @@
+; Z80 Assembly: 8-bit microprocessor language
+; Arithmetic with floating-point not supported natively
+; Placeholder: simulate p = m * v, etc. using fixed-point or external math
+; Display values via serial or LED output
