71f97f8d385106e260325a36e2fb0defb5b2e274
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 19:26:52 +0700

    Update ocaml_snippets.ml

--- examples/ocaml_snippets.ml
+++ examples/ocaml_snippets.ml
@@ -1,6 +1,6 @@
-(* OCaml: functional, fast, used in finance *)
+(* OCaml: functional language used in finance and systems *)
 let () =
   let x = 2.0 and v = 3.0 and m = 5.0 and dm_dt = 0.1 in
   let p = m *. v in
-  let nktg1 = x *. p and nktg2 = dm_dt *. p in
-  Printf.printf "p=%.2f NKTg1=%.2f NKTg2=%.2f\n" p nktg1 nktg2
+  let NKTg1 = x *. p and NKTg2 = dm_dt *. p in
+  Printf.printf "p=%.2f NKTg1=%.2f NKTg2=%.2f\n" p NKTg1 NKTg2
