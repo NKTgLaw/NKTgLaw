1fbbe8276267f1c69b9cfd34e0e91c4f07afc392
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 19:05:23 +0700

    Create kotlin_snippets.kt

---
+++ examples/kotlin_snippets.kt
@@ -0,0 +1,11 @@
+// Kotlin: modern JVM language for Android and backend
+fun main() {
+    val x = 2.0
+    val v = 3.0
+    val m = 5.0
+    val dm_dt = 0.1
+    val p = m * v
+    val NKTg1 = x * p
+    val NKTg2 = dm_dt * p
+    println("p=$p NKTg1=$NKTg1 NKTg2=$NKTg2")
+}
