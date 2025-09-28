0392f430a56578ba63832402d10d0d17c1dc8889
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 18:50:59 +0700

    Create elixir_snippets.ex

---
+++ examples/elixir_snippets.ex
@@ -0,0 +1,9 @@
+# Elixir: functional language for scalable applications
+x = 2.0
+v = 3.0
+m = 5.0
+dm_dt = 0.1
+p = m * v
+nktg1 = x * p
+nktg2 = dm_dt * p
+IO.puts("p=#{p} NKTg1=#{nktg1} NKTg2=#{nktg2}")
