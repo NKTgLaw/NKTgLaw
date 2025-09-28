84dd206c82b725ee573d146620d4a2b5e2f0b013
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 16:45:16 +0700

    Create php_snippets.php

---
+++ examples/php_snippets.php
@@ -0,0 +1,6 @@
+<?php
+// PHP: server-side web development
+$x = 2; $v = 3; $m = 5; $dm_dt = 0.1;
+$p = $m * $v; $NKTg1 = $x * $p; $NKTg2 = $dm_dt * $p;
+echo "p=$p NKTg1=$NKTg1 NKTg2=$NKTg2\n";
+?>
