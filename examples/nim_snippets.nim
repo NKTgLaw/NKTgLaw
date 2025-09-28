5b57d00f1a22b4180d67d818821cc39dbaae9a9f
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 19:23:16 +0700

    Create nim_snippets.nim

---
+++ examples/nim_snippets.nim
@@ -0,0 +1,9 @@
+# Nim: statically typed compiled language with Python-like syntax
+let x = 2.0
+let v = 3.0
+let m = 5.0
+let dm_dt = 0.1
+let p = m * v
+let NKTg1 = x * p
+let NKTg2 = dm_dt * p
+echo "p=", p, " NKTg1=", NKTg1, " NKTg2=", NKTg2
