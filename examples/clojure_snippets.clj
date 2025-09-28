13e38cd61234c86cb62aea3f463b2b4e93009a02
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 18:47:58 +0700

    Create clojure_snippets.clj

---
+++ examples/clojure_snippets.clj
@@ -0,0 +1,9 @@
+;; Clojure: functional language for the JVM
+(let [x 2.0
+      v 3.0
+      m 5.0
+      dm-dt 0.1
+      p (* m v)
+      NKTg1 (* x p)
+      NKTg2 (* dm-dt p)]
+  (println "p=" p "NKTg1=" NKTg1 "NKTg2=" NKTg2))
