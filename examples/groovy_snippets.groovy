4faef606f2a8e871411d1b53cf84513aef1875d1
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 18:01:58 +0700

    Create groovy_snippets.groovy

---
+++ examples/groovy_snippets.groovy
@@ -0,0 +1,6 @@
+// Groovy: dynamic language for the JVM, often used in scripting and automation
+def x = 2.0, v = 3.0, m = 5.0, dm_dt = 0.1
+def p = m * v
+def NKTg1 = x * p
+def NKTg2 = dm_dt * p
+println "p=$p NKTg1=$NKTg1 NKTg2=$NKTg2"
