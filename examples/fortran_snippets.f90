1238af22cf0cbfbc2f3cd3f6615ea8d014c66030
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 17:55:04 +0700

    Create fortran_snippets.f90

---
+++ examples/fortran_snippets.f90
@@ -0,0 +1,9 @@
+! Fortran: khoa học, kỹ thuật, mô phỏng số
+program NKTgLaw
+  real :: x = 2.0, v = 3.0, m = 5.0, dm_dt = 0.1
+  real :: p, NKTg1, NKTg2
+  p = m * v
+  NKTg1 = x * p
+  NKTg2 = dm_dt * p
+  print *, "p=", p, "NKTg1=", NKTg1, "NKTg2=", NKTg2
+end program NKTgLaw
