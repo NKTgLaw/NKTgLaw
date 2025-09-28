46c755420e6b36daebf566ed62cbf18324a3638d
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 17:27:14 +0700

    Create perl_snippets.pl

---
+++ examples/perl_snippets.pl
@@ -0,0 +1,6 @@
+# Perl: scripting, regex-heavy tasks
+my ($x, $v, $m, $dm_dt) = (2.0, 3.0, 5.0, 0.1);
+my $p = $m * $v;
+my $nktg1 = $x * $p;
+my $nktg2 = $dm_dt * $p;
+print "p=$p NKTg1=$nktg1 NKTg2=$nktg2\n";
