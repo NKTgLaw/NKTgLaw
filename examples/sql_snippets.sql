fe13d4e64469286e82ddbdfaf78f8db82a500868
NKTgLaw <traiphieu.com@gmail.com>
2025-09-28 22:04:07 +0700

    Create sql_snippets.sql

---
+++ examples/sql_snippets.sql
@@ -0,0 +1,9 @@
+-- SQL: structured query language for databases
+SELECT
+  2.0 AS x,
+  3.0 AS v,
+  5.0 AS m,
+  0.1 AS dm_dt,
+  5.0 * 3.0 AS p,
+  2.0 * (5.0 * 3.0) AS NKTg1,
+  0.1 * (5.0 * 3.0) AS NKTg2;
