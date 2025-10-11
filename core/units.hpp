5860e9d15f0ac1858daf0decad96b256a6a55d93
Nguyen Khanh Tung <traiphieu.com@gmail.com>
2025-10-11 10:48:12 +0700

    Update units.hpp

--- core/units.hpp
+++ core/units.hpp
@@ -1,1 +1,8 @@
+#pragma once
+#include <string>
+
+namespace units {
+    double to_SI(double value, const std::string& unit);
+    std::string format_unit(double value, const std::string& unit);
+}
 
