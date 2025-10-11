9b1ffdbc6439fe5cdbedf65a9384b1c17f901f35
Nguyen Khanh Tung <traiphieu.com@gmail.com>
2025-10-11 10:49:57 +0700

    Update utils.cpp

--- core/utils.cpp
+++ core/utils.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "utils.hpp"
+
+int main() {
+    std::cout << "=== Testing utils.cpp ===" << std::endl;
+
+    double val = 3.5;
+    if (utils::validate_positive(val)) {
+        utils::log("Valid value: " + std::to_string(val));
+    } else {
+        utils::log("Invalid value");
+    }
+
+    return 0;
+}
 
