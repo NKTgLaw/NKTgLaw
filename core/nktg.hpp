6af53cd30ab7b09c44bfc0b22831008d81049efb
Nguyen Khanh Tung <traiphieu.com@gmail.com>
2025-10-11 10:47:13 +0700

    Update nktg.hpp

--- core/nktg.hpp
+++ core/nktg.hpp
@@ -1,23 +1,7 @@
-#ifndef NKTG_HPP
-#define NKTG_HPP
+#pragma once
 
 namespace nktg {
-
-    // Calculate momentum
-    inline double momentum(double m, double v) {
-        return m * v;
-    }
-
-    // Calculate NKTg₁ = x × p
-    inline double computeNKTg1(double x, double p) {
-        return x * p;
-    }
-
-    // Calculate NKTg₂ = (dm/dt) × p
-    inline double computeNKTg2(double dm_dt, double p) {
-        return dm_dt * p;
-    }
-
+    double momentum(double m, double v);
+    double computeNKTg1(double x, double p);
+    double computeNKTg2(double dm_dt, double p);
 }
-
-#endif
