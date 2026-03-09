7e066578b8b5a297683e57b782b39d6529b8d8bb
Nguyen Khanh Tung <traiphieu.com@gmail.com>
2026-03-09 22:07:38 +0700

    Update nktg.asm

--- clients/asm/src/nktg.asm
+++ clients/asm/src/nktg.asm
@@ -1,24 +1,28 @@
+; -----------------------------------------------------------------------------
+; NKTg Law Core - Assembly Implementation (x86-64)
+; Standardized logic: All calculations are Product Quantities (Multiplication)
+; Expected for (x=2.0, v=3.0, m=5.0, dm_dt=0.1): 
+; compute_nktg1 -> 30.0
+; compute_nktg2 -> 1.5
+; -----------------------------------------------------------------------------
+
 section .text
     global compute_nktg1
     global compute_nktg2
 
-; -----------------------------------------------------------------------------
 ; double compute_nktg1(double x, double v, double m, double dm_dt)
 ; Formula: NKTg1 = x * (m * v)
-; Arguments: xmm0=x, xmm1=v, xmm2=m, xmm3=dm_dt
-; -----------------------------------------------------------------------------
 compute_nktg1:
+    ; Arguments: xmm0=x, xmm1=v, xmm2=m, xmm3=dm_dt
     mulsd xmm1, xmm2    ; xmm1 = m * v (momentum p)
     mulsd xmm0, xmm1    ; xmm0 = x * p (NKTg1)
-    ret                 ; return NKTg1 in xmm0
+    ret                 ; return result in xmm0
 
-; -----------------------------------------------------------------------------
 ; double compute_nktg2(double x, double v, double m, double dm_dt)
 ; Formula: NKTg2 = dm_dt * (m * v)
-; Arguments: xmm0=x, xmm1=v, xmm2=m, xmm3=dm_dt
-; -----------------------------------------------------------------------------
 compute_nktg2:
+    ; Arguments: xmm0=x, xmm1=v, xmm2=m, xmm3=dm_dt
     mulsd xmm1, xmm2    ; xmm1 = m * v (momentum p)
     mulsd xmm3, xmm1    ; xmm3 = dm_dt * p (NKTg2)
-    movapd xmm0, xmm3   ; move result to xmm0 for return
-    ret                 ; return NKTg2 in xmm0
+    movapd xmm0, xmm3   ; move NKTg2 to return register xmm0
+    ret                 ; return result in xmm0
