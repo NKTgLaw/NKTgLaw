2f5c525d8354ca9e665eebf06b9124cee4b9de39
Nguyen Khanh Tung <traiphieu.com@gmail.com>
2026-03-10 17:30:54 +0700

    Update test_nktgLaw.m

--- clients/matlab/tests/test_nktgLaw.m
+++ clients/matlab/tests/test_nktgLaw.m
@@ -3,7 +3,10 @@ function tests = test_nktgLaw
 end
 
 function testBasic(testCase)
+    % Test values: x=2, v=3, m=5, dm_dt=0.1
     [p, NKTg1, NKTg2] = nktgLaw(2, 3, 5, 0.1);
+    
+    % Verify results against Product Logic
     verifyEqual(testCase, p, 15);
     verifyEqual(testCase, NKTg1, 30);
     verifyEqual(testCase, NKTg2, 1.5);
