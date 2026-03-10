c228f1d0d1d118872d3be4bd690f3b9e8a4f0336
Nguyen Khanh Tung <traiphieu.com@gmail.com>
2026-03-10 17:58:12 +0700

    Update queries.sql

--- clients/sql/queries.sql
+++ clients/sql/queries.sql
@@ -1,8 +1,11 @@
 -- Momentum = m * v
+-- Calculation: 5.0 * 3.0 = 15.0
 SELECT id, m * v AS momentum FROM nktg_objects;
 
--- NKTg₁ = m * v + dm_dt * x
-SELECT id, m * v + dm_dt * x AS nktg1 FROM nktg_objects;
+-- NKTg₁ = x * (m * v)
+-- Calculation: 2.0 * 15.0 = 30.0
+SELECT id, x * (m * v) AS nktg1 FROM nktg_objects;
 
--- NKTg₂ = NKTg₁ / m
-SELECT id, (m * v + dm_dt * x) / m AS nktg2 FROM nktg_objects;
+-- NKTg₂ = dm_dt * (m * v)
+-- Calculation: 0.1 * 15.0 = 1.5
+SELECT id, dm_dt * (m * v) AS nktg2 FROM nktg_objects;
