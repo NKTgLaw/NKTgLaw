8ee9ff545d93114de367cbfbec5ceffe61438bf7
Nguyen Khanh Tung <traiphieu.com@gmail.com>
2026-03-10 17:40:06 +0700

    Update setup.py

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -3,9 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name="nktg-law",
     version="1.0.0",
-    description="Python SDK for NKTg Law",
-    author="NKTgLaw",
+    description="Python SDK for NKTg Law (Varying Inertia Model)",
+    author="Nguyễn Khánh Tùng",
+    author_email="traiphieu.com@gmail.com",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "requests>=2.25.0",
+    ],
     python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
 )
